25dc307c7deae42a7f7ee3d3162a93f4fe2dce18
Trent Poche <trent_poche@schumachergroup.com>
2013-12-19 18:52:20 -0600

    Added WriteToFile and UpdateFieldPerms functions to Profile object

--- profiler.go
+++ profiler.go
@@ -2,8 +2,10 @@ package main
 
 import(
 	"os"
+	"errors"
 	"flag"
 	"fmt"
+	"strings"
 	"encoding/xml"
 	"io/ioutil"
 )
@@ -43,28 +45,92 @@ func init() {
 	flag.StringVar(&path, "filepath", ".", "base filepath for the program")
 }
 
-func main() {
-	if path == "" {
+func NewProfileFromFile(filepath string) (*Profile, error) {
+	if filepath == "" {
 		fmt.Println("Error: Base file path undefined!")
-		return
+		return nil, errors.New("NewProfileFromFile: Invalid path")
 	}
 
-	xmlFile, err := os.Open(path + "/profiles/Accounting.profile")
+	xmlFile, err := os.Open(filepath)
 	if err != nil {
 		fmt.Println("Error opening XML file: ", err)
-		return
+		return nil, errors.New("NewProfileFromFile: Error opening file")
 	}
 	defer xmlFile.Close()
 
 	b, err := ioutil.ReadAll(xmlFile)
 	if err != nil {
 		fmt.Println("Error reading XML into byte slice: ", err)
-		return
+		return nil, errors.New("NewProfileFromFile: Error reading file into byte slice")
 	}
 
 	//var p Profile
-	p := new(Profile)
-	xml.Unmarshal(b, p)
+	pro := new(Profile)
+	xml.Unmarshal(b, pro)
+	if err != nil {
+		return nil, errors.New("NewProfileFromFile: Error unmarshaling xml")
+	}
+	
+	return pro, nil
+}
+
+func (p *Profile) WriteToFile(filepath string) (int, error) {
+	// check that path is valid
+	folders := strings.Split(filepath, "/")
+	basepath := strings.Join(folders[:len(folders)-1], "/")
+	_, e := os.Stat(basepath)
+	if os.IsNotExist(e) {
+		return 0, errors.New("WriteProfileToFile: Invalid file path")
+	}
+	// write modified profile to file
+	out, e := xml.MarshalIndent(p, "", "    ")
+	if e != nil {
+		fmt.Printf("Error marshaling XML: %v\n", e)
+		return 0, e
+	}
+
+	fout, e := os.Create(filepath)
+	if e != nil {
+		fmt.Println("Error creating file: ", e)
+		return 0, e
+	}
+
+	nb, e := fout.Write(out)
+	if e != nil {
+		fmt.Println("Error writing file: ", e)
+		return 0, e
+	} 
+
+	return nb, nil
+}
+
+func (p *Profile) UpdateFieldPerms(objName string, rd bool, ed bool) (int, error) {
+	if objName == "" {
+		return 0, errors.New("UpdateFieldPerms: Object name must be specified")
+	}
+	count := 0
+	for i, v := range p.FieldPermList {
+		if strings.HasPrefix(v.Field, objName) {
+			if v.Readable != rd {
+				p.FieldPermList[i].Readable = rd
+				count += 1
+			}
+			if v.Editable != ed {
+				p.FieldPermList[i].Editable = ed
+				count += 1
+			}
+		}		
+	}
+	return count, nil
+}
+
+func main() {	
+	flag.Parse()
+	p, err := NewProfileFromFile(path + "/profiles/Accounting.profile")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	// process field permissions
 	for i, f := range p.FieldPermList {
@@ -79,22 +145,11 @@ func main() {
 		}
 	}
 
-	// write modified profile to file
-	out, err := xml.MarshalIndent(p, "", "    ")
+	n, err := p.WriteToFile(path + "/out/Accounting2.profile")
 	if err != nil {
-		fmt.Printf("Error marshaling XML: %v\n", err)
+		fmt.Println(err)
 		return
 	}
-	fout, err := os.Create(path + "/out/Accounting.profile")
-	if err != nil {
-		fmt.Println("Error creating file: ", err)
-		return
-	}
-	n, err := fout.Write(out)
-	if err != nil {
-		fmt.Println("Error writing file: ", err)
-		return
-	} else {
-		fmt.Println("Updated profile written to file! Number of lines: ", n)
-	}	
+
+	fmt.Printf("Modified profile file created with size: %d\n", n)	
 }
